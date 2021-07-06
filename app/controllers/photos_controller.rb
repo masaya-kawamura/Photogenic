f890921c878bcf21cdeeb414de162a9485b0e6a5
masaya-kawamura <lucky.number0128@gmail.com>
2021-07-06 22:57:16 +0000

    投稿詳細の大枠が完成しました

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,11 +1,24 @@
 class PhotosController < ApplicationController
+
   def new
+    @photo = Photo.new
   end
 
   def create
+    photo = current_user.photos.new(photo_params)
+    if photo.save
+      flash[:notice] = "写真を投稿しました"
+      redirect_to photo_path(photo.id)
+    else
+      flash[:alert] = "写真を投稿できませんでした"
+      @photo = photo
+      render :new
+    end
   end
 
   def show
+    @photo = Photo.find(params[:id])
+    @user = @photo.user
   end
 
   def index
@@ -19,4 +32,11 @@ class PhotosController < ApplicationController
 
   def destroy
   end
+
+    private
+
+    def photo_params
+      params.require(:photo).permit(:photo_image, :story, :detail)
+    end
+
 end
