cb68930392778ae8535657ef68a41e9340933c7f
masaya-kawamura <lucky.number0128@gmail.com>
2021-07-07 16:48:53 +0000

    コメント機能をAjax化しました

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,13 +5,11 @@ class CommentsController < ApplicationController
     comment = current_user.comments.new(comment_params)
     comment.photo_id = @photo.id
     comment.save
-    redirect_to photo_path(@photo)
   end
 
   def destroy
     @photo = Photo.find(params[:photo_id])
     Comment.find_by(photo_id: params[:photo_id], id: params[:id]).destroy
-    redirect_to photo_path(@photo)
   end
 
     private
