39a1662e59ec201c0ddf307ed249ac73c4d6d157
masaya-kawamura <lucky.number0128@gmail.com>
2021-07-25 01:28:01 +0000

    [fix]メール認証の本番環境設定を修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,11 +33,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = true
 
-  if Rails.env.production?
-    config.action_mailer.default_url_options = { host: 'http://52.196.224.85/' }
-  else
-    config.action_mailer.default_url_options = { protcol: 'https', host: 'https://165df428a95044b7b372dba1e3715a17.vfs.cloud9.ap-northeast-1.amazonaws.com/' }
-  end
+  config.action_mailer.default_url_options = { protcol: 'https', host: 'https://165df428a95044b7b372dba1e3715a17.vfs.cloud9.ap-northeast-1.amazonaws.com/' }
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
